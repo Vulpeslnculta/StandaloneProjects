e94db68ea725fe69ca5bafd26b07c49790979fa9
Vulpeslnculta <98029041+Vulpeslnculta@users.noreply.github.com>
2022-01-21 23:33:57 +0100

    Add files via upload
    Patched guess the number game

---
+++ Random.py
@@ -0,0 +1,24 @@
+import random
+check = 0
+number = random.randint(1, 10)
+answer = int(input("Make your guess: \n"))
+print("_" * 30)
+while answer != number:
+    if answer == 0:
+        check += 1
+        print("Are you afraid? That's why you quit I guess")
+        break
+    elif answer < number:
+        print("Guess higher next time!")
+        answer = int(input("Try again!\n"))
+        print("_" * 30)
+    elif answer > number:
+        print("Guess lower next time!")
+        answer = int(input("Try again!\n"))
+        print("_" * 30)
+    else:
+
+        break
+if check == 0:
+    print("You got it!")
+
